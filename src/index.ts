a5ad047d85d4b2db87520e4d7a8bf96eb2e3ddc8
Beto Muniz <contato@betomuniz.com>
2023-10-25 00:32:05 -0300

    adicionando funcao fetchOpenGraphImage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 import jsdom from "jsdom";
+
+const fetchOpenGraphImage = async (url: string) => {
+  const page = await fetch(url);
+  const html = await page.text();
+
+  // needed to fix https://github.com/jsdom/jsdom/issues/2230
+  const virtualConsole = new jsdom.VirtualConsole();
+  virtualConsole.on("error", () => {});
+
+  const dom = new jsdom.JSDOM(html, { virtualConsole });
+
+  const image = dom.window.document.querySelector('meta[property="og:image"]');
+
+  const imageContent = image?.getAttribute("content");
+
+  return imageContent;
+};
